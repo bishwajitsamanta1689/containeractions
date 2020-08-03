d35416aaf43ac59c401cc362c6f73e31b6919ef2
bishwajitsamanta1689 <bishwajitsamanta1689@gmail.com>
2020-08-03 09:43:32 +0530

    Initial Shell Scripts

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Hello $INPUT_MYINPUT"
+memory=$(cat /proc/meminfo)
+
+# Setting Variables for Github Actions Workflow to Work
+
+# Github Syntax for Setting the output Parameter to our Output in our Actions
+# It should be within :: ::
+# And Finally we can access the variable with $
+# This will help to call upon in the Workflow file.
+
+echo "::set-output name=memory::$memory"
